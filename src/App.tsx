2a11125aca89c246a4d4199edff469d3f7d18a18
shreyanithin <shreyanithinr@gmail.com>
2025-01-25 20:17:55 +0530

    pushed shreyas app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+// Import React Router and Home component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';  // Make sure the Home component is correctly imported
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />  {/* Home page route */}
+        {/* You can add more routes here for other pages */}
+      </Routes>
+      
+      
+    </Router>
+  );
+};
 
-export default App
+export default App;
